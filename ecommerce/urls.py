399a6a485512efcbe7f96b2fd1fc193257146900
Michael Santos <mcialbr123@gmail.com>
2023-07-06 11:25:00 -0300

    /contato

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import index, contato
 
 urlpatterns = [
     path('', index),
+    path('contato/', contato),
     path('admin/', admin.site.urls)
 ]
