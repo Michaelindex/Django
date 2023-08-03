e38ef398273e335869b8594803d26b036e51d11d
Michael Santos <mcialbr123@gmail.com>
2023-08-03 09:31:17 -0300

    update

--- Python/hamburgueria/core/models.py
+++ Python/hamburgueria/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome=models.Charfield('Nome', max_length=100)
+    preco=models.DecimalFeidl('Preco', decimal_places=2, max_digits=8)
+    ingredientes = models.TextField('Ingredientes', max_lenght=300)
+    def __str__(self):
+        return self.nome
