3116b04a124eaf395b7d487ddf698ce6a75fd4d5
Michael Santos <mcialbr123@gmail.com>
2023-08-03 09:53:15 -0300

    att

--- Python/hamburgueria/core/models.py
+++ Python/hamburgueria/core/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Produto(models.Model):
-    nome=models.Charfield('Nome', max_length=100)
-    preco=models.DecimalFeidl('Preco', decimal_places=2, max_digits=8)
-    ingredientes = models.TextField('Ingredientes', max_lenght=300)
+    nome=models.CharField('Nome', max_length=100)
+    preco=models.DecimalField('Preco', decimal_places=2, max_digits=8)
+    ingredientes = models.TextField('Ingredientes', max_length=300)
     def __str__(self):
         return self.nome
