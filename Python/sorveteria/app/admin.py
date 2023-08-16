cab263c95f6dbc5722bd49e4c9fbe3d4f2a979ac
Michael Santos <mcialbr123@gmail.com>
2023-08-16 10:58:09 -0300

    Update admin.py

--- Python/sorveteria/app/admin.py
+++ Python/sorveteria/app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import cliente, Produto
 
+class ListaCliente
 # Register your models here.
-admin.site.register(cliente)
+admin.site.register(cliente, ListaCliente)
 admin.site.register(Produto)
