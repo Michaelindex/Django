f995a9e6f2f6dc2153bd8a014937a677075df7d7
Michael Santos <mcialbr123@gmail.com>
2023-08-16 11:13:16 -0300

    att

--- Python/sorveteria/app/admin.py
+++ Python/sorveteria/app/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import cliente, Produto
 
-class ListaCliente
+class ListaCliente(admin.ModelAdmin):
+    list_display=('id', 'nome', 'cpf', 'logradouro', 'cidade')
+    list_display_links=('id', 'nome')
+    search_fields=('nome', 'cpf', 'logradouro', 'cidade')
+    list_filter=('cidade',)
+    list_editable=('logradouro', 'cidade')
+    list_per_page=2
 # Register your models here.
 admin.site.register(cliente, ListaCliente)
 admin.site.register(Produto)
